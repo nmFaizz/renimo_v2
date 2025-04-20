5ca44d530225747e93f68514aee9042fe753c572
unknown <ilznorz582@gmail.com>
2025-04-20 11:40:49 +0700

    fix: profile page params doesnt satisfy PageProps

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -2,9 +2,9 @@ import UserProfileContainer from "@/app/profile/[username]/containers/UserProfil
 
 export default async function UserProfilePage({ 
     params,
-}: Readonly<{
+}: {
     params: { username: string };
-}>) {
+}) {
     const { username } = await params
 
     return (
